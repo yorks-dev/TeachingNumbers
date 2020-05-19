b50f64bebc047bc1cc27a348074ee2e39d08c2ad
yorks-dev <63572464+yorks-dev@users.noreply.github.com>
2020-05-20 03:44:52 +0530

    Added the bash output

---
+++ output.bash
@@ -0,0 +1,267 @@
+[edward@Edward TeachingNumbers]$ ./TeachingNumbers 
+0 0
+midleRight : .
+bottom : 1
+num : 1
+1 0
+----------------
+top : 0
+topRight : .
+midleRight : 2
+num : 2
+1 1
+----------------
+topLeft : 0
+top : .
+topRight : .
+midleLeft : 1
+midleRight : .
+bottomLeft : 3
+num : 3
+2 0
+----------------
+top : 1
+topRight : 2
+midleRight : .
+num : 3
+2 0
+----------------
+0 0
+midleRight : .
+bottom : 2
+bottomRight : 1
+num : 1
+1 1
+----------------
+topLeft : 0
+top : .
+topRight : .
+midleLeft : 2
+num : 2
+1 0
+----------------
+top : 0
+topRight : .
+midleRight : 1
+bottom : 3
+num : 3
+2 0
+----------------
+top : 2
+topRight : 1
+midleRight : .
+num : 3
+2 0
+----------------
+0 1
+midleLeft : .
+midleRight : 1
+num : 1
+0 2
+----------------
+midleLeft : 0
+midleRight : 2
+num : 2
+0 3
+----------------
+midleLeft : 1
+bottomLeft : 3
+num : 3
+1 2
+----------------
+topLeft : 0
+top : 1
+topRight : 2
+midleLeft : .
+midleRight : .
+bottomLeft : 4
+num : 4
+2 1
+----------------
+topLeft : .
+top : .
+topRight : 3
+midleLeft : .
+midleRight : .
+bottomLeft : 5
+num : 5
+3 0
+----------------
+top : .
+topRight : 4
+midleRight : .
+bottom : .
+bottomRight : 6
+num : 6
+4 1
+----------------
+topLeft : 5
+top : .
+topRight : .
+midleLeft : .
+midleRight : .
+bottomLeft : .
+bottom : .
+bottomRight : 7
+num : 7
+5 2
+----------------
+topLeft : 6
+top : .
+topRight : .
+midleLeft : .
+midleRight : .
+bottomLeft : .
+bottom : 9
+bottomRight : 8
+num : 8
+6 3
+----------------
+topLeft : 7
+top : .
+midleLeft : 9
+num : 9
+6 2
+----------------
+topLeft : .
+top : 7
+topRight : .
+midleLeft : .
+midleRight : 8
+num : 9
+6 2
+----------------
+0 1
+midleLeft : .
+midleRight : 1
+num : 1
+0 2
+----------------
+midleLeft : 0
+midleRight : 3
+bottomLeft : .
+bottom : 2
+num : 2
+1 2
+----------------
+topLeft : 0
+top : 1
+topRight : 3
+num : 3
+0 3
+----------------
+midleLeft : 1
+bottomLeft : 2
+bottom : 4
+num : 4
+1 3
+----------------
+topLeft : 1
+top : 3
+midleLeft : 2
+bottomLeft : .
+bottom : .
+num : 4
+1 3
+----------------
+3 2
+topLeft : 6
+top : .
+topRight : 1
+num : 1
+2 3
+----------------
+topLeft : 7
+top : 8
+midleLeft : .
+bottomLeft : 0
+bottom : 2
+num : 2
+3 3
+----------------
+topLeft : .
+top : 1
+midleLeft : 0
+bottomLeft : 4
+bottom : 3
+num : 3
+4 3
+----------------
+topLeft : 0
+top : 2
+midleLeft : 4
+num : 4
+4 2
+----------------
+topLeft : 5
+num : 5
+3 1
+----------------
+topLeft : .
+top : 6
+num : 6
+2 1
+----------------
+topLeft : .
+top : .
+topRight : 7
+num : 7
+1 2
+----------------
+topLeft : .
+top : .
+topRight : .
+midleLeft : .
+midleRight : 8
+num : 8
+1 3
+----------------
+topLeft : .
+top : .
+midleLeft : 7
+bottomLeft : .
+bottom : 1
+num : 8
+1 3
+----------------
+0 1
+midleLeft : 1
+num : 1
+0 0
+----------------
+midleRight : 0
+num : 1
+0 0
+----------------
+0 9
+midleLeft : 1
+num : 1
+0 8
+----------------
+midleLeft : 2
+num : 2
+0 7
+----------------
+midleLeft : 3
+num : 3
+0 6
+----------------
+midleLeft : 4
+num : 4
+0 5
+----------------
+midleLeft : 5
+num : 5
+0 4
+----------------
+midleLeft : 6
+num : 6
+0 3
+----------------
+midleLeft : 8
+midleRight : 5
+num : 6
+0 3
+----------------
+===============================================================================
+All tests passed (7 assertions in 1 test case)
